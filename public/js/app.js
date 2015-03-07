92148af28380ecfacadc4303bcb9f78c7c8fed47
jaballadares <jballadares28@gmail.com>
2015-03-06 17:47:56 -0800

    happy

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 //   $(this).append("<textarea></textarea>");
 // });
 $('.circles').on('click', function() {
-  $(this).replaceWith("<div class='chatContainer'>" + "<textarea></textarea>" + "<button class='buttonTime'>Shh</button>" + "</div");
+  $(this).replaceWith("<div class='chatContainer'>" +"<p class='description'></p>" + "<textarea placeholder='This is not a chat App. \n I REPEAT THIS IS NOT A CHAT APP'></textarea>" + "<button class='buttonTime'>Shh</button>" + "</div");
   $('button').on('click', function() {
     $(this).after("hooray");
   });
