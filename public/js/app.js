291cbb1a605dfed8f72e7ff33e9c1df36d5f0533
jaballadares <jballadares28@gmail.com>
2015-03-13 01:40:48 -0500

    working at sxsw

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
+$('.circles').on('click', function() {
+  var chatModule = "<div class='chatContainer'>"  +
+                   "<p class='description'></p>" +
+                   "<textarea placeholder='This is not a chat App. \n I REPEAT THIS IS NOT A CHAT APP'></textarea>" +
+                   "<button class='buttonTime'>Shh</button>" +
+                   "</div>";
+  $(this).replaceWith(chatModule);
+});
+
+$('.buttonTime').on('click', function() {
+  $(this).after("<p>hooray</p>");
+});
+
+
 // $('.circles').on('click', function() {
 //   $(this).append("<textarea></textarea>");
 // });
@@ -13,15 +27,3 @@
 //     $(this).after("hooray");
 //   });
 // });
-$('.circles').on('click', function() {
-  var chatModule = "<div class='chatContainer'>"  +
-                   "<p class='description'></p>" +
-                   "<textarea placeholder='This is not a chat App. \n I REPEAT THIS IS NOT A CHAT APP'></textarea>" +
-                   "<button class='buttonTime'>Shh</button>" +
-                   "</div>";
-  $(this).replaceWith(chatModule);
-});
-
-$('button').on('click', function() {
-    $(this).after("hooray");
-});
